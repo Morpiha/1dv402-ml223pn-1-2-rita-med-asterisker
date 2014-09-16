07daf3a1f7fbdfaf06d9a2ef71ad2149534a38bd
morpiha <morpiha@live.se>
2014-09-16 14:57:09 +0200

    Commit 1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+         
+                for (int j = 0; j < 39; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                if(i%2==0)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
